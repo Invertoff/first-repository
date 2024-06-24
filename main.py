aeed82fdab747de5dc7d86878f5b0339dca019ae
Your Name <invert2012@gmail.com>
2024-06-24 04:53:22 +0300

    hcjhvcjv

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello World")
-print("Hello Git")
+def format_string(string, length):
+    if len(string) >= length:
+        return string
+    else:
+        space = (length - len(string)) // 2
+        return " " * space + string
