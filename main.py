ad28e936e7d73aa9e930817b8e8ace81c97b81f7
Your Name <invert2012@gmail.com>
2024-06-30 07:23:21 +0300

    birthdays_checks

--- main.py
+++ main.py
@@ -1,6 +1,56 @@
-def format_string(string, length):
-    if len(string) >= length:
-        return string
-    else:
-        space = (length - len(string)) // 2
-        return " " * space + string
+from datetime import datetime, date, timedelta
+
+
+def string_to_date(date_string):
+    return datetime.strptime(date_string, "%Y.%m.%d").date()
+
+
+def date_to_string(date):
+    return date.strftime("%Y.%m.%d")
+
+
+def prepare_user_list(user_data):
+    prepared_list = []
+    for user in user_data:
+        prepared_list.append({"name": user["name"], "birthday": string_to_date(user["birthday"])})
+    return prepared_list
+
+
+def find_next_weekday(start_date, weekday):
+    days_ahead = weekday - start_date.weekday()
+    if days_ahead <= 0:
+        days_ahead += 7
+    return start_date + timedelta(days=days_ahead)
+
+
+def adjust_for_weekend(birthday):
+    if birthday.weekday() >= 5:
+        return find_next_weekday(birthday, 0)
+    return birthday
+
+
+def get_upcoming_birthdays(users, days=7):
+    upcoming_birthdays = []
+    today = date.today()
+
+    for user in users:
+        birthday_this_year = user["birthday"].replace(year=today.year)
+
+        if today > birthday_this_year:
+            birthday_this_year = user["birthday"].replace(year = today.year +1)
+                    
+
+        if 0 <= (birthday_this_year - today).days <= days:
+            congratulation_date = adjust_for_weekend(birthday_this_year)
+            congratulation_date_str = date_to_string(congratulation_date)
+            upcoming_birthdays.append({"name": user["name"], "congratulation_date": congratulation_date_str})
+
+    return upcoming_birthdays
+users = [
+    {"name": "Bill Gates", "birthday": "1955.10.28"},
+    {"name": "Steve Jobs", "birthday": "1955.02.24"},
+    {"name": "Jane Smith", "birthday": "1990.04.27"},
+]
+
+prepared_users = prepare_user_list(users)
+print(get_upcoming_birthdays(prepared_users, 7))
